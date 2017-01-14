e80acfe24cf942a84b9bd8e6e585e44e78e42052
abhishek97 <zapper.awake@gmail.com>
2017-01-14 15:07:14 +0530

    Fixed interval and error report

--- index.js
+++ index.js
@@ -9,39 +9,75 @@ const helper = require('sendgrid').mail;
 const moment = require('moment');
 
 
-let intervalTime = 20000;
+let intervalTime = 30000;
 let anyProblem = false;
 
 const hosts = [
     {
         hostname : "google.com",
-        description : "Public Google .com"
+        description : "Public Google .com",
+        anyProblem : false
     },
     {
         hostname: "103.82.44.1",
-        description: "ANB Core (CCR-1009)"
+        description: "ANB Core (CCR-1009)",
+        anyProblem : false
     },
     {
         hostname : "103.82.44.6",
-        description : "ANB NAS INDERLOK (RB3011 UiaS)"
+        description : "ANB NAS INDERLOK (RB3011 UiaS)",
+        anyProblem : false
     },
     {
         hostname : "103.82.44.128",
-        description : "Synnefo"
+        description : "Synnefo",
+        anyProblem : false
     }
 ];
 
 
 function checkHosts() {
-    anyProblem  = false;
     hosts.forEach(function (host) {
         ping.sys.probe(host.hostname , function (isAlive) {
+            if(isAlive && host.anyProblem )
+            {
+                let from = new helper.Email("networkmonitor@alphanetbroadband.com"),
+                    to = new helper.Email("sales@alphanetbroadband.com"),
+                    subject = "Network WatchDog Ping Ok",
+                    content = new helper.Content("text/html" , `
+            Ping to the following host is <b>Up</b> now: ${host.hostname}  </br>
+            Description : ${host.description}
+            Last checked : ${moment().format("dddd, MMMM Do YYYY, h:mm:ss a")}
+            `);
+
+                const mail = new helper.Mail(from , subject , to , content);
+                const sg = require('sendgrid')(config.sendgrindAPI);
+                const request = sg.emptyRequest({
+                    method : 'POST',
+                    path : '/v3/mail/send',
+                    body : mail.toJSON()
+                });
+                sg.API(request , function (err , response) {
+                    if(err)
+                    {
+                        console.error("cannot send host email. Error : " , err);
+                    }
+                    else
+                    {
+                        console.log("Email Sent!");
+                        intervalTime = 30000;
+                    }
+                });
+
+                host.anyProblem = false;
+                return ;
+            }
             if(isAlive)
                 return;
 
 
-            anyProblem = true;
-            let from = "network-monitor@alphanetbroadband.com",
+            host.anyProblem = true;
+            let from = new helper.Email("networkmonitor@alphanetbroadband.com"),
                 to = new helper.Email("sales@alphanetbroadband.com"),
                 subject = "Network WatchDog Ping Failed",
                 content = new helper.Content("text/html" , `
@@ -57,7 +93,6 @@ function checkHosts() {
                 path : '/v3/mail/send',
                 body : mail.toJSON()
             });
-
             sg.API(request , function (err , response) {
                 if(err)
                 {
@@ -66,15 +101,11 @@ function checkHosts() {
                 else
                 {
                     console.log("Email Sent!");
-                    intervalTime = 2000;
+                    intervalTime = 30000;
                 }
             });
         });
     });
-
-
-    if(! anyProblem )
-        intervalTime = 20000;
 
 
     setTimeout(checkHosts , intervalTime );
